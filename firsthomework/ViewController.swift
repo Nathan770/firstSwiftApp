425ae2923fd08d56416012f3014b21e379df9d75
nathan amiel <nanathanamiel5@gmail.com>
2022-04-25 18:34:44 +0300

    Initial Commit

---
+++ firsthomework/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  firsthomework
+//
+//  Created by Nathan Amiel on 25/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
